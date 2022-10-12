a4a1bf3657c56dec73f0d6e2dc1cf08733b5bcc2
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2022-10-12 22:30:25 +0200

    Update main.c

--- Exo1_TD4/main.c
+++ Exo1_TD4/main.c
@@ -2,59 +2,70 @@
 #include <stdlib.h>
 typedef int Tab[100];
 
-
-void tri_tab(Tab t, int *n)
+void affichage(Tab t, int *n)
 {
-    int *temp;
-    int i,j;
-    for(i=0;i<*(n-1);i++)
+    int i;
+    for(i=0;i<*n;i++)
     {
-        for(j=i;j<*n;j++)
+        printf("%d",t[i]);
+    }
+}
+void tri_tab(Tab t, int n)
+{
+    int temp;
+    int i,j;
+    for(i=0;i<(n-1);i++)
+    {
+        for(j=i;j<n;j++)
         {
-            if(t[i]>t[j])
+            if(*(t+i)>*(t+j))
             {
-                *temp=t[i];
-                t[i]=t[j];
-                t[j]=*temp;
+                temp=*(t+i);
+                *(t+i)=*(t+j);
+                *(t+j)=temp;
 
             }
         }
     }
 }
-int dicho(Tab t, int *c, int *g, int *d,int *r) //il faut que le tableau soit trié
+int dicho(Tab t, int c, int g, int d, int * r) //il faut que le tableau soit triÃ©
 {
-    *r=*(t+((*g+*d)/2));
-    if (*(t+*r)==*c)
+    tri_tab(t,g);
+    int ret= g + (d-g)/2;
+    if (t[ret] == c)
     {
-        return *r;
+        *r=ret;
     }
     else
     {
-        if(*c<*(t+*r))
+        if(t[ret] > c)
         {
-            dicho(t,&c,&g,&r,&r);
-            return *r;
+             dicho(t,c,g,ret,r); // ou return dicho
         }
         else
         {
 
-        if(*c>*(t+*r))
-        {
-            dicho(t,&c,&r,&d,&r);
-            return *r;
-        }
-        else
-        {
-            return -1;
+            if(t[ret] < c)
+            {
+                 dicho(t,c,ret,d,r);
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
+return;
 }
-}
+
 
 int main()
 {
-    int *r=0;
+    int r=0;
+    printf("%d",r);
     Tab t={2,3,6,7,8,9,5,7};
-    tri_tab(t,7);
-    printf("%d",dicho(t,9,0,7,&r));
+    //tri_tab(t,7);
+   // affichage(t,7);
+    dicho(t,7,0,7,&r);
+    printf("%d",r);
 }
