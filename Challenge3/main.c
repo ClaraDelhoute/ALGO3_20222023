6b9c001ebb876f7ac0cb81f0ac1c45162406bef9
ClaraDelhoute <113034191+ClaraDelhoute@users.noreply.github.com>
2022-10-12 14:48:29 +0200

    Update main.c

--- Challenge3/main.c
+++ Challenge3/main.c
@@ -11,7 +11,7 @@ int maximun(int n, Tab t)
         {
             if(t[n]<t[n-1])
             {
-               return max=maximun(n-1,t);
+               return max=maximun(n-1,t); //return doit se placer ici
             }
             else
             {
@@ -20,10 +20,12 @@ int maximun(int n, Tab t)
         }
 }
 
-    int main()
+int main()
 {
     int n; Tab t;
     Tab table={8,9,7};
      n = 3;
     printf("%d",maximun(n, table));
+    Tab t2={9,14,12,11,1,7,5};
+     printf("%d",maximun(n, t2));
 }
